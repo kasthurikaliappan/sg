b6be567615cefe3fc6bafda422d7cf8fd153ef7b
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-07-06 14:35:03 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,17 @@
+package gk;
+import java.util.*;
+public class array {
+  public static void main(String args[]) {
+	  int i,t=3;
+	  Scanner s=new Scanner(System.in);
+	  int n=s.nextInt();
+	  int a[]=new int[n];
+	  for(i=0; i<n;i++) {
+		  a[i]=s.nextInt();
+		  }
+	  for(i=0; i<n; i++) {
+		  t=t^a[i];
+	  }
+	  System.out.println(t);
+  }
+}
